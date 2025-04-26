bc060ea9a0bb8457e8cc318a94a2dcc8c5a27549
ASHIQ02KHAN <ashiq2khan02@gmail.com>
2025-04-26 21:02:09 +0600

    Find Bridges in a Graph(Tarjan’s Algorithm)A bridge in an undirected graph is an edge that, if removed, increases the number of connected components.Ex:1 - 0 - 2,Removing edge 0-1 splits the graph!

---
+++ Contest Type Qus/Q4.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> adj;
+vector<int> tin, low;
+vector<bool> visited;
+int timer;
+
+void dfs(int u, int parent) {
+    visited[u] = true;
+    tin[u] = low[u] = timer++;
+
+    for (int v : adj[u]) {
+        if (v == parent) continue;
+        if (visited[v]) {
+            low[u] = min(low[u], tin[v]);
+        } else {
+            dfs(v, u);
+            low[u] = min(low[u], low[v]);
+            if (low[v] > tin[u]) {
+                cout << "Bridge: " << u << " - " << v << endl;
+            }
+        }
+    }
+}
+
+void findBridges(int n) {
+    timer = 0;
+    tin.assign(n, -1);
+    low.assign(n, -1);
+    visited.assign(n, false);
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i])
+            dfs(i, -1);
+    }
+}
+
+int main() {
+    int n = 5;
+    adj.resize(n);
+    adj[0] = {1, 2};
+    adj[1] = {0, 2};
+    adj[2] = {0, 1, 3};
+    adj[3] = {2, 4};
+    adj[4] = {3};
+
+    findBridges(n);
+    return 0;
+}
